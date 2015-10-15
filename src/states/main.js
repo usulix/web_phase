7baa065e15b1fc3a12a687567ce78f35fd2f881e
David Hayes <drhayes@gmail.com>
2015-10-14 21:43:36 -0500

    Don't forget the explosions.

--- src/states/main.js
+++ src/states/main.js
@@ -1,9 +1,12 @@
 'use strict';
 
+import Explosions from '../managers/explosions';
 import Player from '../sprites/player';
 
 export default class Main extends Phaser.State {
   create() {
+    let explosions = new Explosions(this.game);
+
     let player = new Player(this.game, 100, 100);
     this.add.existing(player);
   }
